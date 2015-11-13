31313e02ba77bb9bfb56748df842d5024768e1eb
etpinard <etienne.t.pinard@gmail.com>
2015-11-13 16:34:10 -0500

    add bundle of topojsons

---
+++ index.js
@@ -0,0 +1,14 @@
+exports['africa_110m.json'] = require('./dist/africa_110m.json');
+exports['africa_50m.json'] = require('./dist/africa_50m.json');
+exports['asia_110m.json'] = require('./dist/asia_110m.json');
+exports['asia_50m.json'] = require('./dist/asia_50m.json');
+exports['europe_110m.json'] = require('./dist/europe_110m.json');
+exports['europe_50m.json'] = require('./dist/europe_50m.json');
+exports['north-america_110m.json'] = require('./dist/north-america_110m.json');
+exports['north-america_50m.json'] = require('./dist/north-america_50m.json');
+exports['south-america_110m.json'] = require('./dist/south-america_110m.json');
+exports['south-america_50m.json'] = require('./dist/south-america_50m.json');
+exports['usa_110m.json'] = require('./dist/usa_110m.json');
+exports['usa_50m.json'] = require('./dist/usa_50m.json');
+exports['world_110m.json'] = require('./dist/world_110m.json');
+exports['world_50m.json'] = require('./dist/world_50m.json');
