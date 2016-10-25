537d9c0767ef946446b8adb7ac526c13db73fb9b
Étienne Tétreault-Pinard <etienne.t.pinard@gmail.com>
2016-10-25 00:06:35 -0400

    try bumping timeout

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -19,6 +19,8 @@ module.exports = function(config) {
 
         singleRun: isCI,
 
+        browserNoActivityTimeout: 100000,
+
         browserify: {
             watch: true,
             debug: true
