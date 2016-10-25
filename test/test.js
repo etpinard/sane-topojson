80894664b8201a1710b8912eda7231c5438d53b7
Étienne Tétreault-Pinard <etienne.t.pinard@gmail.com>
2016-10-24 23:36:55 -0400

    add mucho tests

--- test/test.js
+++ test/test.js
@@ -3,6 +3,13 @@
 var saneTopojson = require('../');
 var topojson = require('topojson');
 
+var ITEM_WITH_NO_SUBUNITS = [
+    'africa_110m', 'africa_50m',
+    'asia_110m', 'asia_50m',
+    'europe_110m', 'europe_50m',
+    'south-america_110m'
+];
+
 describe('sane topojson general', () => {
 
     it('should have correct test environments', () => {
@@ -31,42 +38,96 @@ describe('sane topojson general', () => {
         ]);
     });
 
+    it('should have all the correct layers', () => {
+        Object.keys(saneTopojson).forEach((k) => {
+            var saneTopojsonItem = saneTopojson[k];
+
+            expect(Object.keys(saneTopojsonItem)).toEqual([
+                'type', 'objects', 'arcs', 'transform', 'bbox'
+            ]);
+
+            expect(Object.keys(saneTopojsonItem.objects)).toEqual([
+                'coastlines', 'land', 'ocean', 'lakes',
+                'rivers', 'countries', 'subunits'
+            ]);
+        });
+    });
+
 });
 
 describe('sane topojson with d3-geo & topojson', () => {
 
-    var width = 960,
-        height = 500;
-
     beforeEach(() => {
-        this.svg = d3.select('body').append('svg');
+        this.svg = d3.select('body').append('svg')
+            .attr('width', 960)
+            .attr('height', 500)
     });
 
     afterEach(() => {
         document.body.removeChild(this.svg.node());
     });
 
-    it('', () => {
-        var projection = d3.geo.equirectangular()
-            .scale(140)
-            .translate([width / 2, height / 2])
-            .precision(0.1);
+    describe('should be able to draw path all layers', () => {
 
-        var path = d3.geo.path().projection(projection);
+        Object.keys(saneTopojson).forEach((k) => {
+            var saneTopojsonItem = saneTopojson[k];
 
-        var saneTopojsonItem = saneTopojson['world_110m'];
+            Object.keys(d3.geo).forEach((p) => {
+                var projFunc = d3.geo[p];
 
-        var datum = topojson.feature(
-            saneTopojsonItem,
-            saneTopojsonItem.objects.land
-        );
+                if(!(
+                    typeof projFunc === 'function' &&
+                    projFunc.raw !== undefined
+                )) return;
 
-        this.svg.append('path')
-            .datum(datum)
-            .attr('d', path);
+                it('in topojson '+ k + ' using projection function ' + p, () => {
+                    appendLayers(this.svg, projFunc, saneTopojsonItem);
 
-        expect(d3.selectAll('path').size()).toEqual(1);
-        expect(d3.select('path').attr('d').length).toEqual(184040);
+                    var paths = this.svg.selectAll('path');
+
+                    expect(paths.size()).toEqual(7);
+
+                    paths.each(function() {
+                        var path = d3.select(this),
+                            l = path.attr('id');
+
+                        if(
+                            l === 'subunits' &&
+                            ITEM_WITH_NO_SUBUNITS.indexOf(k) !== -1
+                        ) {
+                            expect(path.attr('d')).toBe(null);
+                        }
+                        else {
+                            expect(path.attr('d').length > 1).toBe(true);
+                        }
+                    })
+                });
+            });
+        });
+
     });
 
 });
+
+function appendLayers(svg, projFunc, saneTopojsonItem) {
+    var width = +svg.attr('width');
+    var height = svg.attr('height');
+
+    var projection = projFunc()
+        .translate([width / 2, height / 2])
+        .precision(0.1);
+
+    var path = d3.geo.path().projection(projection);
+
+    Object.keys(saneTopojsonItem.objects).forEach((l) => {
+        var datum = topojson.feature(
+            saneTopojsonItem,
+            saneTopojsonItem.objects[l]
+        );
+
+        svg.append('path')
+            .datum(datum)
+            .attr('d', path)
+            .attr('id', l)
+    });
+}
