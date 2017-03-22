c7d920773a775e839b499546840dbf2b4339701b
silky <noonsilk+-noonsilk@gmail.com>
2017-03-23 08:27:40 +1100

    required 10m data in lib

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 exports['world_110m'] = require('./dist/world_110m.json');
 exports['world_50m'] = require('./dist/world_50m.json');
+exports['world_10m'] = require('./dist/world_10m.json');
 
 exports['africa_110m'] = require('./dist/africa_110m.json');
 exports['africa_50m'] = require('./dist/africa_50m.json');
+exports['africa_10m'] = require('./dist/africa_10m.json');
 
 exports['asia_110m'] = require('./dist/asia_110m.json');
 exports['asia_50m'] = require('./dist/asia_50m.json');
+exports['asia_10m'] = require('./dist/asia_10m.json');
 
 exports['europe_110m'] = require('./dist/europe_110m.json');
 exports['europe_50m'] = require('./dist/europe_50m.json');
+exports['europe_10m'] = require('./dist/europe_10m.json');
 
 exports['north-america_110m'] = require('./dist/north-america_110m.json');
 exports['north-america_50m'] = require('./dist/north-america_50m.json');
+exports['north-america_10m'] = require('./dist/north-america_10m.json');
 
 exports['south-america_110m'] = require('./dist/south-america_110m.json');
 exports['south-america_50m'] = require('./dist/south-america_50m.json');
+exports['south-america_10m'] = require('./dist/south-america_10m.json');
 
 exports['usa_110m'] = require('./dist/usa_110m.json');
 exports['usa_50m'] = require('./dist/usa_50m.json');
+exports['usa_10m'] = require('./dist/usa_10m.json');
